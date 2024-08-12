8c79f4fae009c8c3c1fddfd6e58dd594a455062a
gufranalam226 <63334077+gufranalam226@users.noreply.github.com>
2024-08-12 18:51:50 +0530

    configuring CORS

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from "express"
 import router from "./routes/routes.js"
+import cors from "cors"
 
 const app = express()
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    optionsSuccessStatus: 200 
+}))
 
 app.use("/", router)
 
